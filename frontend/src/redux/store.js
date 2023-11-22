fea61a6976b93875499c16057ed8d5fe1b629f11
Chandra Srinivas P <chandrasrinivas12@gmail.com>
2023-11-22 09:44:35 +0530

    first commit

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { legacy_createStore as createStore } from "redux";
 import { reducers } from "./reducers";
 
+const enableReduxDevTools = false; // Set this to false to disable Redux DevTools
+
 const mystore = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  enableReduxDevTools
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    : undefined
 );
 
 export default mystore;
